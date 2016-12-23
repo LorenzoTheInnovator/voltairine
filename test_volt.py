56f91b6c5eaa432703e102d2d1ab8cf1b93f8213
jrabbit <jackjrabbit@gmail.com>
2016-12-23 11:38:53 -0500

    there's my nail! :hammer:

--- test_volt.py
+++ test_volt.py
@@ -9,29 +9,34 @@ import voltairine
 #     def test_cowsay(self):
 #         pass
 
-async def do_nothing(channel, msg):
-    await asyncio.sleep(1)
+async def do_nothing_msg(channel, msg):
+    await asyncio.sleep(0)
+
+async def do_nothing_wait(author=None):
+    await asyncio.sleep(0)
 
 class BulkTestResponses(unittest.TestCase):
-    @mock.patch("discord.Client.send_message", wraps=do_nothing)
+    @mock.patch("discord.Client.send_message", wraps=do_nothing_msg)
     def test_responses(self, patched_send):
-        # commands = help_msg.split("\n")
-        commands = ['!4chan', "!benned3"]
+        commands = help_msg.split("\n")
+        # commands = ['!4chan', "!benned3"]
         for command in commands:
             # if command in ["!help", "!cowsay", "!cowthink"]:
             #     print("cowardly not testing these")
+            if not command.startswith("!encounter"):
+                msg = mock.MagicMock()
+                type(msg).content = mock.PropertyMock(return_value=command)
+                msg.channel.return_value = "maketotaldestroy"
+                msg.message.author.return_value = "somefucko"
+                # print("calling {}".format(command))
+                loop = asyncio.new_event_loop()
+                asyncio.set_event_loop(loop)
+                loop.run_until_complete(on_message(msg))
+                loop.close()
+                last_call = patched_send.call_args
+                # print(last_call)
 
-            msg = mock.MagicMock()
-            msg.content.return_value = command
-            msg.channel.return_value = "maketotaldestroy"
-            msg.message.author.return_value = "somefucko"
-            print("calling {}".format(command))
-            loop = asyncio.new_event_loop()
-            asyncio.set_event_loop(loop)
-            # voltairine.client.send_message = do_nothing
-            loop.run_until_complete(on_message(msg))
-            loop.close()
-            print(patched_send.mock_calls)
+                self.assertIsInstance(last_call[0][1], str)
 
         # for command in help_msg.split("\n"):
         #     if command not in  ["!help", "!cowsay", "!cowthink"]:
