fd438a304f28ae604bcafa0f91a3e3cd4ecc4b9f
Botanist <botanybay3.14@gmail.com>
2017-01-27 16:50:39 -0500

    tweaked changelog
    made the changelog names bold

--- voltairine.py
+++ voltairine.py
@@ -124,7 +124,7 @@ help_msg = ('''\
 !vote''')
 
 changelog = ('''
-v0.1.1
+**v0.1.1**
 -changed !gold to !inventory
 -Added shop
 -Added whisper
@@ -133,13 +133,13 @@ v0.1.1
 -Added encounters
 -Added potions
 -Added fresh and wtf
-v0.1.2
+**v0.1.2**
 -added dozens of new commands
 -added cowsay
-v0.1.3
+**v0.1.3**
 -added pyborg
 -removed benned commands, now with Emma
-v0.1.4
+**v0.1.4**
 -added catsnake command
 -added Foucault command
 -fixed anticiv command
