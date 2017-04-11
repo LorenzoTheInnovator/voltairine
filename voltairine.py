43e2a27cf7a37f4085bde200007058137e391b83
Botanist <botanybay3.14@gmail.com>
2017-04-11 17:52:27 -0400

    updated help command :rat:

--- voltairine.py
+++ voltairine.py
@@ -36,32 +36,7 @@ compliment3 = ["Pilot", "Canoe", "Captain", "Pirate", "Hammer", "Knob", "Box", "
 ball = ['It is certain','It is decidedly so','Without a doubt','Yes, definitely','You may rely on it','As I see it, yes','Most likely','Outlook good','Yes','Signs point to yes','Reply hazy try again','Ask again later','Better not tell you now','Cannot predict now','Concentrate and ask again','Don\'t count on it','My reply is no','My sources say no','Outlook not so good','Very doubtful']
 help_msg = ('''\
 
-**Full List**: http://bit.do/c7a9k
-
-**INFO**
-!afaq
-!anarchism
-!ancom
-!anfem
-!anti-civ
-!bakunin
-!berkman
-!bonanno
-!bookchin
-!bookclub
-!books
-!chomsky
-!dejacque
-!durruti
-!feminism
-!foucault
-!goldman
-!kropotkin
-!makhno
-!most
-!mutualism
-!proudhon
-!stirner
+**Full List**: http://gooseberrycollective.net/bots/commands.html
 
 ''')
 
